34d7887ec468e86cb9df76b4936734d67726a090
Benedikt Böhm <bb@xnull.de>
2022-02-02 17:36:37 +0100

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "s3_bucket_arn" {
   value       = aws_s3_bucket.main.arn
 }
 
-output "cloudfront_distribution_arns" {
-  description = "ARNs of the created cloudfront distributions"
-  value       = aws_cloudfront_distribution.main[*].arn
+output "cloudfront_distribution_arn" {
+  description = "ARN of the created cloudfront distribution"
+  value       = aws_cloudfront_distribution.main.arn
 }
