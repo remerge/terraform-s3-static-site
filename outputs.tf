dfb32af8ea1381fe9d3affc6ebd486af9e7ee326
Benedikt Böhm <bb@xnull.de>
2022-02-06 10:39:06 +0100

    remove broken outputs

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_arn" {
-  description = "ARN of the created bucket"
-  value       = aws_s3_bucket.main.arn
-}
-
-output "cloudfront_distribution_arn" {
-  description = "ARN of the created cloudfront distribution"
-  value       = aws_cloudfront_distribution.main.arn
-}
